cc536fb009ab65f1d7049a8acbe6c0f00b7d2dfe
Howarddj <66848380+Howarddj@users.noreply.github.com>
2021-08-06 15:41:21 +0100

    Add files via upload

---
+++ device.mk
@@ -0,0 +1,44 @@
+
+LOCAL_PATH := device/sony/pdx206
+
+# define hardware platform
+PRODUCT_PLATFORM := kona
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+#TEST
+# A/B support
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.1-impl-wrapper.recovery \
+    android.hardware.boot@1.1-impl-wrapper \
+    android.hardware.boot@1.1-impl-recovery \
+    bootctrl.kona \
+    bootctrl.kona.recovery \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+
+PRODUCT_HOST_PACKAGES += \
+    libandroidicu
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
